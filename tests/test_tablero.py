1878a571f7902710f9a8a2f1d9fbad7316a25a55
Brunix3017 <brunomedinaagnini@gmail.com>
2023-09-24 14:23:40 -0500

    Test bellman

--- tests/test_tablero.py
+++ tests/test_tablero.py
@@ -1,7 +1,7 @@
 from app.classes import Tablero, Vertice, TipoVertice
 
 lista_tablero = [[3, 2, 3], [3, 3, 4], [1, 3, 4]]
-
+bellman_ejemplo = [[0, 2, 3], [3, 0, 4], [1, 3, 0]]
 def test_tablero():
     vertices = [
         Vertice("00", TipoVertice.NORMAL), Vertice("01", TipoVertice.FINAL), Vertice("02", TipoVertice.NORMAL),
@@ -21,3 +21,12 @@ def test_getitem_tablero():
 def test_get_vertice_tablero():
     tablero = Tablero(lista_tablero, parse=True)
     assert tablero.get_vertice("12") == Vertice("12", TipoVertice.OBSTACULO)
+
+def test_bellman_ford():
+    inicio, final = 0, 2
+    camino = [0, 2]
+    peso = 3
+    resultado = Tablero.bellman_ford(bellman_ejemplo, inicio, final)
+    caminoBellman, pesoBellman = resultado
+    assert caminoBellman == camino
+    assert pesoBellman == peso
