aaf7a957de028d1b80297d6089ea8f437814d8a1
Brunix3017 <brunomedinaagnini@gmail.com>
2023-10-22 16:47:23 -0500

    Algoritmos arreglados

--- app/classes/tablero.py
+++ app/classes/tablero.py
@@ -1,6 +1,7 @@
 from .vertice import Vertice, TipoVertice
-import sys
-INF = sys.maxsize
+import heapq as hq
+INF = float("inf")
+
 class Tablero:
     DISTANCIAS = {
         TipoVertice.INICIO: 0, #Peso
@@ -36,64 +37,87 @@ class Tablero:
     def __str__(self):
         return f"({self.filas}x{self.columnas})\n{[fila for fila in self.tablero]}"
     
-    def getPath(self, vecino, salida, llegada):
+    def getPath(self, vecino, inicio, final):
         camino = []
-        actual = llegada
-        while actual != salida:
-            camino.insert(0, actual)
-            actual = vecino[actual]
-        camino.insert(0, salida)
+        while final != inicio:
+            camino.insert(0, final)
+            final = vecino[final]
+        camino.insert(0, inicio)
         return camino
     
-    def bellman_ford(self, Inicio, Final):
-        n = len(self.tablero)
-        distancia = [INF] * n
-        vecino = [-1] * n
-        distancia[Inicio] = 0
+    def bellman_ford(self, start, end):
+        # Crear una lista de nodos únicos
+        nodes = set()
+        graph = self.tablero
+        for u, neighbors in graph.items():
+            nodes.add(u)
+            for v in neighbors:
+                nodes.add(v)
+
+        n = len(nodes)
+        distancia = {node: INF for node in nodes}
+        vecino = {node: -1 for node in nodes}
+        distancia[start] = 0
 
         for _ in range(n - 1):
-            for u in range(n):
-                for v in range(n):
-                    if distancia[u] != INF and distancia[u] + self.DISTANCIAS[self.tablero[u][v]] < distancia[v]:
-                        distancia[v] = distancia[u] + self.DISTANCIAS[self.tablero[u][v]]
+            for u, neighbors in graph.items():
+                for v, peso in neighbors.items():
+                    if distancia[u] != INF and distancia[u] + peso < distancia[v]:
+                        distancia[v] = distancia[u] + peso
                         vecino[v] = u
 
-        for u in range(n):
-            for v in range(n):
-                if distancia[u] != INF and distancia[u] + self.DISTANCIAS[self.tablero[u][v]] < distancia[v]:
-                    print('Negative-weight cycle is found!!')
+        for u, neighbors in graph.items():
+            for v, peso in neighbors.items():
+                if distancia[u] != INF and distancia[u] + peso < distancia[v]:
+                    print('¡Se ha encontrado un ciclo de peso negativo!')
                     return
 
-        if distancia[Final] != INF:
-            camino = self.getPath(vecino, Inicio, Final)
-            peso_total = distancia[Final]
-            return camino, peso_total
+        if distancia[end] != INF:
+            camino = self.getPath(vecino, start, end)
+            return camino, distancia[end]
         else:
             return None
 
-    def Floyd_Warshall(self, inicio, final):
-        n = len(self.tablero)
-        dist = [list(row) for row in self.tablero]
+    def a_star(self, start, end, h):
+        G = self.tablero
+        n = len(G)
+        g = {node: INF for node in G}
+        visited = {node: False for node in G}
+        f = {node: INF for node in G}
+        path = {node: None for node in G}
 
-        for k in range(n):
-            for i in range(n):
-                for j in range(n):
-                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+        g[start] = 0
+        f[start] = h[start]
+        q = []
+        hq.heappush(q, (f[start], start))
 
-        # Verificar si hay un camino entre los vértices inicio y final
-        if dist[inicio][final] == INF:
-            return None  # No hay camino
-
-        # Reconstruir el camino
-        camino = []
-        actual = inicio
-        while actual != final:
-            camino.append(actual)
-            for vecino in range(n):
-                if vecino != actual and dist[actual][vecino] + self.DISTANCIAS[self.tablero[vecino][final]] == dist[actual][final]:
-                    actual = vecino
+        while q:
+            _, n = hq.heappop(q)
+            if not visited[n]:
+                visited[n] = True
+                if n == end:
                     break
-        camino.append(final)
-        # Calcular el peso total del camino
-        peso_total = dist[inicio][final]
-        return camino, peso_total
+                for v, w in G[n].items():
+                    if not visited[v] and g[n] + w < g[v]:
+                        path[v] = n
+                        g[v] = g[n] + w
+                        f[v] = g[v] + h[v]
+                        hq.heappush(q, (f[v], v))
+
+        if path[end] is None:
+            return None, INF  # No se encontró un camino
+
+        # Reconstruir el camino y calcular el peso
+        camino_minimo = []
+        node = end
+        peso = 0
+        while node is not None:
+            camino_minimo.insert(0, node)
+            if path[node] is not None:
+                peso += G[node][path[node]]
+            node = path[node]
+
+        return camino_minimo, peso
+
+    def manhattan(self, node1, node2):
+        return abs(node1 - node2)
--- tests/test_tablero.py
+++ tests/test_tablero.py
@@ -1,9 +1,26 @@
 from app.classes import Tablero, Vertice, TipoVertice
-import sys
-INF = sys.maxsize
+INF = float("inf")
 lista_tablero = [[3, 2, 3], [3, 3, 4], [1, 3, 4]]
-bellman_ejemplo = [[INF, 2, 3], [3, INF, 4], [1, 3, INF]]
-floyd_ejemplo = [[0, 2, 3], [3, 0, 4], [1, 3, 0]]
+
+grafo_no_dirigido = { #Ejemplo de A star con diccionario 
+    1: {2: 3, 4: 5},
+    2: {1: 3, 3: 2, 5: 1},
+    3: {2: 2, 6: 4},
+    4: {1: 5, 5: 2, 7: 1},
+    5: {2: 1, 4: 2, 6: 3, 8: 1},
+    6: {3: 4, 5: 3, 9: 2},
+    7: {4: 1, 8: 3, 10: 5},
+    8: {5: 1, 7: 3, 9: 2, 11: 4},
+    9: {6: 2, 8: 2, 12: 1},
+    10: {7: 5, 11: 1, 13: 3},
+    11: {8: 4, 10: 1, 12: 2, 14: 3},
+    12: {9: 1, 11: 2, 15: 4},
+    13: {10: 3, 14: 5},
+    14: {11: 3, 13: 5, 15: 1},
+    15: {12: 4, 14: 1, 16: 2},
+    16: {15: 2}
+}
+
 def test_tablero():
     vertices = [
         Vertice("00", TipoVertice.NORMAL), Vertice("01", TipoVertice.FINAL), Vertice("02", TipoVertice.NORMAL),
@@ -25,21 +42,22 @@ def test_get_vertice_tablero():
     assert tablero.get_vertice("12") == Vertice("12", TipoVertice.OBSTACULO)
 
 def test_bellman_ford():
-    inicio, final = 0, 2
-    camino = [0, 2]
-    peso = 3
-    tablero = Tablero(bellman_ejemplo, parse=True)
+    inicio, final = 1, 16
+    camino = [1, 2, 5, 8, 9, 12, 15, 16]
+    peso = 14
+    tablero = Tablero(grafo_no_dirigido, parse=True)
     resultado = tablero.bellman_ford(inicio, final)
     caminoBellman, pesoBellman = resultado
     assert caminoBellman == camino
     assert pesoBellman == peso
 
-def test_floyd_warshall():
-    inicio, final = 0, 2
-    camino = [0, 2]
-    peso = 3
-    tablero = Tablero(floyd_ejemplo, parse=True)
-    resultado = tablero.Floyd_Warshall(inicio, final)
-    caminoFloyd, pesoFloyd = resultado
-    assert caminoFloyd == camino
-    assert pesoFloyd == peso 
+def test_A_star():
+    inicio, final = 1, 16
+    camino = [1, 2, 5, 8, 9, 12, 15, 16]
+    peso = 14
+    tablero = Tablero(grafo_no_dirigido, parse=True)
+    h = tablero.manhattan(inicio, final)
+    resultado = tablero.a_star(inicio, final, h)
+    caminoA, pesoA = resultado
+    assert camino == caminoA
+    assert peso == pesoA
